ee73e95c15f0f14da05a0929a4e267cc89dfec17
Felipe Cordeiro <imfelipe.cordeiro12@gmail.com>
2023-07-03 23:34:09 -0300

    add dependences

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
-Future<void> main() async {
+import 'app_state.dart';
+import 'home_page.dart';
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ApplicationState(),
+    builder: ((context, child) => const App()),
+  ));
 }
 
 class MyApp extends StatelessWidget {
